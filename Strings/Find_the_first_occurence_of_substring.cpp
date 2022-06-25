45dacd27879449ea5e5e525fb0caab82e1202e22
Baewada <s12345678901994@gmail.com>
2022-06-25 19:48:34 +0530

    day_7

---
+++ Strings/Find_the_first_occurence_of_substring.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+    int n = x.size();
+   for(int i=0; i<s.size(); i++) {
+       if(x == s.substr(i,n)) {
+           return i;
+       }
+   }
+   return -1;
+}
